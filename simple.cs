bc7d506384f725389ba04da27ef777bf07ccd96f
JackyShen <mebusw@163.com>
2016-08-25 12:56:28 +0800

    Remove loop

--- simple.cs
+++ simple.cs
@@ -4,14 +4,9 @@ class Author {
     public string Company { get; set;}
 
     static public IEnumerable<String> TwitterHandles(IEnumerable<Author> authors, string company) {
-        var result = new List<String> ();
         var loopStart = authors
             .Where(a => a.Company == company)
             .Select(a => a.TwitterHandle)
             .Where (h => h != null); /***/
-        foreach (string handle in loopStart) {
-                result.Add(handle);
-        }
-        return result;
     }
 }
