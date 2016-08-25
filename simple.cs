0f78594cceaeffa6a5c79b0546948473583ebb3b
JackyShen <mebusw@163.com>
2016-08-25 12:36:03 +0800

    Extract Method

--- simple.cs
+++ simple.cs
@@ -5,7 +5,8 @@ class Author {
 
     static public IEnumerable<String> TwitterHandles(IEnumerable<Author> authors, string company) {
         var result = new List<String> ();
-        foreach (Author a in authors) {
+        var loopStart = authors; /***/
+        foreach (Author a in loopStart) {
             if (a.Company == company) {
                 var handle = a.TwitterHandle;
                 if (handle != null)
